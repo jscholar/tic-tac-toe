e287fb0f982d59b9876d6669ca6cc37e94b9d2e1
Jin Cai <jinruncai@gmail.com>
2020-03-02 11:16:08 -0800

    Set up MVC boilerplate

---
+++ app.js
@@ -0,0 +1,38 @@
+/***********************************/
+/*******        MODEL        *******/
+/***********************************/
+
+const Model = (function() {
+    return {};
+})();
+
+/***********************************/
+/*******        VIEW         *******/
+/***********************************/
+
+const View = (function() {
+    return {};
+})();
+
+/***********************************/
+/*******      CONTROLLER     *******/
+/***********************************/
+
+const Contoller = (function() {
+    return {};
+})();
+
+/**********************************/
+
+const App = (function() {
+
+    const initialize = function() {
+        console.log('Tic-tac-toe has initialized');
+    }
+
+    return {
+        initialize
+    };
+})();
+
+App.initialize();
