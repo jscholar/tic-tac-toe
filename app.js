82d33bf85f2060cb65b95c2421af82670c512b30
Jin Cai <jinruncai@gmail.com>
2020-03-02 15:58:09 -0800

    Check for a winning board

--- app.js
+++ app.js
@@ -18,13 +18,78 @@ const Model = (function() {
     const resetGameState = function() {
         state.board = emtpyBoard.slice();
         state.currentPlayer = 'X';
+        state.finished = false;
+        state.winner = null;
+        state.movesPlayed = 0;
     };
 
     const makeMove = function(row, column) {
+        row = Number(row);
+        column = Number(column);
         state.board[row][column] = state.currentPlayer;
         View.updateSquare(row, column, state.currentPlayer);
+
+        let waysWon = checkWinningBoard(state.board, row, column, state.currentPlayer);
+        if (waysWon.length > 0) {
+            // Won game
+            console.log(waysWon);
+        };
+
         state.currentPlayer = state.currentPlayer === 'X' ? 'O' : 'X';
     };
+
+    /**
+     * Check board for winning state after a move
+     */
+    const checkWinningBoard = function(board, row, column, player) {
+        const waysWon = [];
+        
+        // Check winning row
+        let winningRow = true;
+        for (let c = 0; c < board[row].length; c++) {
+            if (board[row][c] !== player) {
+                winningRow = false;
+                break;
+            }
+        }
+        if (winningRow) waysWon.push('winningRow');
+        
+        // Check winning column
+        let winningColumn = true;
+        for (let r = 0; r < board.length; r++) {
+            if (board[r][column] !== player) {
+                winningColumn = false;
+                break;
+            }
+        }
+        if (winningColumn) waysWon.push('winningColumn');
+        
+        // Check winning major diagonal
+        if (row === column) {
+            let winningMajorDiagonal = true;
+            for (let r = 0; r < board.length; r++) {
+                if (board[r][r] !== player) {
+                    winningMajorDiagonal = false;
+                    break;
+                }
+            }
+            if (winningMajorDiagonal) waysWon.push('winningMajorDiagonal');
+        }
+        
+        // Check winning minor diagonal
+        if ((board.length - 1) - row === column) {
+            let winningMinorDiagonal = true;
+            for (let r = 0; r < board.length; r++) {
+                if (board[r][(board.length - 1) - r] !== player) {
+                    winningMinorDiagonal = false;
+                    break;
+                }
+            }
+            if (winningMinorDiagonal) waysWon.push('winningMinorDiagonal');
+        }
+
+        return waysWon;
+    }
     
     return {
         initialize,
@@ -103,9 +168,6 @@ const View = (function() {
      * @param {Number} n Length of outer array
      */
     const flattenIndex = function(i, j, n) {
-        i = Number(i);
-        j = Number(j);
-        n = Number(n);
         return i * n + j;
     }
     
